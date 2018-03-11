dbe4526e5d8de6be7a50756c76dfe67d856e4d24
anthonyrojas <anthonyrojas095@gmail.com>
2018-03-10 22:54:55 -0800

    added socket events

--- socketEvents.js
+++ socketEvents.js
@@ -1,3 +1,23 @@
 module.exports = (io)=>{
-    
+    io.on('connection', (socket)=>{
+        socket.on('enter conversation', (conversation)=>{
+            socket.join(conversation);
+        });
+
+        socket.on('leave conversation', (conversation)=>{
+            socket.leave(conversation);
+        });
+
+        socket.on('new message', (conversation)=>{
+            io.sockets.in(coversation).emit('refresh messages', conversation);
+        });
+
+        socket.on('new conversation', (conversation)=>{
+            io.sockets.in(conversation).emit('refresh invitations', conversation);
+        });
+
+        socket.on('disconnect', ()=>{
+            console.log('user disconnected');
+        });
+    })
 }
